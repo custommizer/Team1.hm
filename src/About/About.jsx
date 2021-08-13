39334f60e2d74d79fe85be034cfcf8a2d4359e6c
ArmanDev667 <armansadykov997@gmail.com>
2021-08-14 00:11:58 +0600

    added some sections in about-page

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -7,43 +7,114 @@ import img4 from "../mainpage/images/about-img-4.jpg";
 
 function About() {
   return (
-    <div className="about">
-      <div className="container">
-        <div className="about__title">
-          <h2>About us</h2>
+    <div className="wrapper">
+      <div className="parallax">
+        <div className="parallax__body">
+          <div className="container parallax__container">
+            <div className="parallax__label">About Us</div>
+            <h1 className="parallax__title">
+              Здесь вы сможете подробнее <br /> ознокомиться с историей <br />{" "}
+              нашей комапании
+            </h1>
+          </div>
+          <div className="parallax__image"></div>
         </div>
-        <div className="about__inner">
-          <div className="about__card__res">
-            <img src={img3} alt="loading..." />
-          </div>
-          <div className="about__text">
-            <p>
-              <span>H&M</span> is a Swedish multinational clothing-retail
-              company known for its fast-fashion clothing for men, women,
-              teenagers, and children. As of November 2019, H&M operates in 74
-              countries with over 5,000 stores under the various company brands,
-              with 126,000 full-time equivalent positions. It is the
-              second-largest global clothing retailer, behind Spain-based
-              Inditex (parent company of Zara). Founded by Erling Persson and
-              run by his son Stefan Persson and Helena Helmersson, the company
-              makes its online shopping available in 33 countries.
-            </p>
-          </div>
-          <div className="about__cards">
-            <div className="about__card__line">
-              <div className="about__card">
-                <img src={img1} alt="loading..." />
-              </div>
-              <div className="about__card">
-                <img src={img2} alt="loading..." />
-              </div>
+      </div>
+      <div className="about">
+        <div className="container">
+          <div className="about__content">
+            <div className="about__title">
+              <h2>About us</h2>
             </div>
-            <div className="about__card__line">
-              <div className="about__card">
+            <div className="about__inner">
+              <div className="about__card__res">
                 <img src={img3} alt="loading..." />
               </div>
-              <div className="about__card">
-                <img src={img4} alt="loading..." />
+              <div className="about__text">
+                <p>
+                  <span>H&M</span> - шведская международная компания по
+                  розничной торговле одеждой, известная своей модной одеждой для
+                  мужчин, женщин, подростков и детей. По состоянию на ноябрь
+                  2019 года H&M работает в 74 странах, имея более 5000 магазинов
+                  под различными брендами компании, что эквивалентно 126 000
+                  должностей с полной занятостью. Это второй по величине
+                  глобальный розничный торговец одеждой после испанской Inditex
+                  (материнской компании Zara). Основанная Эрлингом Перссоном и
+                  управляемая его сыном Стефаном Перссоном и Хеленой Хелмерссон,
+                  компания делает покупки в Интернете доступными в 33 странах.
+                </p>
+              </div>
+              <div className="about__cards">
+                <div className="about__card__line">
+                  <div className="about__card">
+                    <img src={img1} alt="loading..." />
+                  </div>
+                  <div className="about__card">
+                    <img src={img2} alt="loading..." />
+                  </div>
+                </div>
+                <div className="about__card__line">
+                  <div className="about__card">
+                    <img src={img3} alt="loading..." />
+                  </div>
+                  <div className="about__card">
+                    <img src={img4} alt="loading..." />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="about__history">
+              <div className="container">
+                <div className="about__history__content">
+                  <div className="about__history__title">
+                    <h1>История</h1>
+                  </div>
+                  <div className="about__history__text">
+                    <p className="about__history__parag">
+                      Компания была основана Эрлингом Перссоном в 1947 году,
+                      когда он открыл свой первый магазин в Вестеросе, Швеция.
+                      Магазин, под названием Хеннес (по-шведски "ее"), продается
+                      эксклюзивно Женская одежда. Магазин был открыт в Норвегии
+                      в 1964 году. 1968: Persson приобретает розничную торговлю
+                      охотничьей одеждой. Мауриц Видфорс в Стокгольме, что
+                      привело к включению коллекции мужской одежды по
+                      ассортименту и названию перейти на Hennes & Mauritz.
+                    </p>
+                    <p className="about__history__parag">
+                      Компания была зарегистрирована на Стокгольмской фондовой
+                      бирже в 1974. Вскоре, в 1976 году, первый магазин на улице
+                      Скандинавия открылась в Лондоне. H&M продолжала
+                      расширяться в Европе и начал продавать в Интернете в 1998
+                      году, когда купил домен hm.com от компании A1 в
+                      неопубликованном транзакции домена. Двухбуквенный домен
+                      был зарегистрирован в начале 1990-х годов, но данные о
+                      первых регистрация потеряна. Открытие первого магазина в
+                      США. 31 марта 2000 г. на Пятой авеню в Нью-Йорке отмечен
+                      начало экспансии за пределы Европы.
+                    </p>
+                    <p className="about__history__parag">
+                      В 2008 году компания объявила в пресс-релизе, что начнет
+                      продавать предметы домашнего обихода. Изначально
+                      распространяется через онлайн-каталог компании, есть
+                      теперь магазины H&M Home расположены по всему миру.
+                      Следующий экспансия в Азии и на Ближнем Востоке и запуск
+                      концептуальные магазины, включая COS, Weekday, Monki и
+                      Cheap Понедельник, в 2009 и 2010 годах, консалтинговая
+                      компания Interbrand. поставил компанию на двадцать первое
+                      место в рейтинге самых ценных глобальных брендов, что делает
+                      его лидером розничной торговли в опрос. Его стоимость
+                      оценивалась в 12–16 миллиардов долларов.
+                    </p>
+                    <p className="about__history__parag">
+                      На конец 2011 г. у H&M находилось 2325 магазинов, а на
+                      конец 2011 г. - 2629 магазинов. магазинов в конце августа
+                      2012 года. Открылся 3000-й магазин. в сентябре 2013 года в
+                      Чэнду, Китай. В октябре 2020 года H&M объявили, что
+                      планируют закрыть 5% своих магазинов по всему миру в 2021
+                      году в результате COVID-19 пандемии.
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
