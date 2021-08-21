4abfe457e919f14da8d6ba4cc9b5fd410adb54ab
ArmanDev667 <armansadykov997@gmail.com>
2021-08-22 00:45:03 +0600

    added some adaptability

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,43 @@
-import Header from './header/Header';
-import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Main from './mainpage/Main';
-import About from './About/About';
-import Faq from './Faq/Faq';
-import Client from './client/Client';
-import Store from './store/Store'
-import Contacts from './contact/Contacts'
+import Header from "./header/Header";
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Main from "./mainpage/Main";
+import About from "./About/About";
+import Faq from "./Faq/Faq";
+import Client from "./client/Client";
+import Store from "./store/Store";
+import Contacts from "./contact/Contacts";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Header />
+    <BrowserRouter>
+      <Header />
 
+      <Route exact path="/">
+        <Main />
+      </Route>
 
-        <Switch>
-          <Route path="/main">
-            <Main />
-          </Route>
-          <Route path="/about-page">
-            <About />
-          </Route>
-          <Route path="/faq">
-            <Faq />
-          </Route>
-          <Route path="/client">
-            <Client />
-          </Route>
-          <Route path="/store">
-            <Store />
-          </Route>
-          <Route path="/contacts">
-            <Contacts />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
+      <Switch>
+        <Route path="/main">
+          <Main />
+        </Route>
+        <Route path="/about-page">
+          <About />
+        </Route>
+        <Route path="/faq">
+          <Faq />
+        </Route>
+        <Route path="/client">
+          <Client />
+        </Route>
+        <Route path="/store">
+          <Store />
+        </Route>
+        <Route path="/call">
+          <Contacts />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
