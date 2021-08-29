c726e41088900a9a4c897b557f3051ce856e7cb9
kenadil <180113017@stu.sdu.edu.kz>
2021-08-30 03:35:15 +0600

    fixes

--- src/mainpage/Main.jsx
+++ src/mainpage/Main.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
+import React from "react";
 import "./main.css";
-import Intro from './Intro/Intro';
-import Clothes from '../ListOfClothes/Clothes';
-import FlexClothes from '../FlexClothes/FlexClothes';
+import Intro from "./Intro/Intro";
+import Clothes from "../ListOfClothes/Clothes";
+import FlexClothes from "../FlexClothes/FlexClothes";
 
-function Main(){
-    return(
-        <div className="main">
-            <Intro />
-            <Clothes />
-            <FlexClothes />
-        </div>
-
-    )
+function Main() {
+  return (
+    <div className="main">
+      <Intro />
+      <Clothes />
+      <FlexClothes />
+    </div>
+  );
 }
 export default Main;
